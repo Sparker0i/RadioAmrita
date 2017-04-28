d055ecb8debafbd50dc2476c7afbe53dd93d4cc1
Aaditya Menon <AadityaMenon007@gmail.com>
2017-04-28 18:28:54 +0530

    Fixed Splash Not Showing Error

--- app/src/main/java/in/ac/amrita/radioamrita/SplashScreen.java
+++ app/src/main/java/in/ac/amrita/radioamrita/SplashScreen.java
@@ -1,9 +1,14 @@
 package in.ac.amrita.radioamrita;
 
+import android.content.Context;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.sql.Time;
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -13,13 +18,33 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        MC global = (MC) getApplicationContext();
-        try {
-            global.init();
-        }
-        catch (InterruptedException | ExecutionException | TimeoutException ex) {
-
-        }
+        final MC global = (MC) getApplicationContext();
+        final Context context = this;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    global.init();
+                }
+                catch (TimeoutException tex) {
+                    new MaterialDialog.Builder(context)
+                            .title("Check Internet Connection")
+                            .content("Please check whether you are connected to AMRITA Networks or not, and then restart the app")
+                            .positiveText("OK")
+                            .onPositive(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                    finish();
+                                    System.exit(0);
+                                }
+                            })
+                            .show();
+                }
+                catch (InterruptedException | ExecutionException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } , 500);
     }
 }
 
